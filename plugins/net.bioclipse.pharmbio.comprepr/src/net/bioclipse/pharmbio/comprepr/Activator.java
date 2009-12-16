f8323e4a540cce7efd0145f0e4120cb1733e2323
Egon Willighagen <egon.willighagen@gmail.com>
2009-12-16 17:19:21 +0100

    Added an empty helper manager for the PharmBio Computer Representation chapter

---
+++ plugins/net.bioclipse.pharmbio.comprepr/src/net/bioclipse/pharmbio/comprepr/Activator.java
@@ -0,0 +1,105 @@
+/*******************************************************************************
+ * Copyright (c) 2009  Egon Willighagen <egon.willighagen@gmail.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contact: http://www.bioclipse.net/
+ ******************************************************************************/
+package net.bioclipse.pharmbio.comprepr;
+
+import net.bioclipse.pharmbio.comprepr.business.ICompreprManager;
+import net.bioclipse.pharmbio.comprepr.business.IJavaCompreprManager;
+import net.bioclipse.pharmbio.comprepr.business.IJavaScriptCompreprManager;
+
+import org.apache.log4j.Logger;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+import org.osgi.util.tracker.ServiceTracker;
+
+/**
+ * The Activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+
+    private static final Logger logger = Logger.getLogger(Activator.class);
+
+    // The shared instance
+    private static Activator plugin;
+
+    // Trackers for getting the managers
+    private ServiceTracker javaFinderTracker;
+    private ServiceTracker jsFinderTracker;
+
+    public Activator() {
+    }
+
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+        javaFinderTracker
+            = new ServiceTracker( context,
+                                  IJavaCompreprManager.class.getName(),
+                                  null );
+
+        javaFinderTracker.open();
+        jsFinderTracker
+            = new ServiceTracker( context,
+                                  IJavaScriptCompreprManager.class.getName(),
+                                  null );
+
+        jsFinderTracker.open();
+    }
+
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
+
+    /**
+     * Returns the shared instance
+     *
+     * @return the shared instance
+     */
+    public static Activator getDefault() {
+        return plugin;
+    }
+
+    public ICompreprManager getJavaCompreprManager() {
+        ICompreprManager manager = null;
+        try {
+            manager = (ICompreprManager)
+                      javaFinderTracker.waitForService(1000*10);
+        }
+        catch (InterruptedException e) {
+            throw new IllegalStateException(
+                          "Could not get the Java CompreprManager",
+                          e );
+        }
+        if (manager == null) {
+            throw new IllegalStateException(
+                          "Could not get the Java CompreprManager");
+        }
+        return manager;
+    }
+
+    public IJavaScriptCompreprManager getJavaScriptCompreprManager() {
+        IJavaScriptCompreprManager manager = null;
+        try {
+            manager = (IJavaScriptCompreprManager)
+                      jsFinderTracker.waitForService(1000*10);
+        }
+        catch (InterruptedException e) {
+            throw new IllegalStateException(
+                          "Could not get the JavaScript CompreprManager",
+                          e );
+        }
+        if (manager == null) {
+            throw new IllegalStateException(
+                          "Could not get the JavaScript CompreprManager");
+        }
+        return manager;
+    }
+}
